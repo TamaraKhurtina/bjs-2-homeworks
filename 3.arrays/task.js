80fac974156eae86e838bc7343116fcbdfc5bdff
Tamara Khurtina <toma.shabunina@gmail.com>
2026-08-18 00:11:33 +0700

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,21 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  return arr1.every((element, index) => element === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const usersWithGender = users.filter((user) => user.gender === gender);
+
+  if (usersWithGender.length === 0) {
+    return 0;
+  }
+
+  const ages = usersWithGender.map((user) => user.age);
+
+  const sum = ages.reduce((acc, age) => acc + age, 0);
+
+  return sum / ages.length;
 }
